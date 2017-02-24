560c85eabcb97d31bf199fb4706975662bc0442c
Tayyab Ahmed <harkamalsingh8@outlook.com>
2017-02-24 12:09:11 +0530

    server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-﻿require('rootpath')();
+require('rootpath')();
 var express = require('express');
 var app = express();
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var expressJwt = require('express-jwt');
 var config = require('config.json');
-
+var port = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: false }));
