c636f65eaf01ea8a20e30e548d2d0248c9f0360d
Tayyab Ahmed <harkamalsingh8@outlook.com>
2017-02-24 11:32:43 +0530

    ss

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# mean-stack-registration-login-example
+heroku-node
+===========
 
-MEAN Stack User Registration and Login Example Application
-
-To see a demo and further details go to http://jasonwatmore.com/post/2015/12/09/MEAN-Stack-User-Registration-and-Login-Example.aspx
+Code for the tutorial by @sevilayha: Deploying Node Apps to Heroku
--- server.js
+++ server.js
@@ -1,32 +1,23 @@
-﻿require('rootpath')();
 var express = require('express');
 var app = express();
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var expressJwt = require('express-jwt');
-var config = require('config.json');
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
 
-// use JWT auth to secure the api
-app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
 
-// routes
-app.use('/login', require('./controllers/login.controller'));
-app.use('/register', require('./controllers/register.controller'));
-app.use('/app', require('./controllers/app.controller'));
-app.use('/api/users', require('./controllers/api/users.controller'));
+// set the home page route
+app.get('/', function(req, res) {
 
-// make '/app' default route
-app.get('/', function (req, res) {
-    return res.redirect('/app');
+	// ejs render automatically looks in the views folder
+	res.render('index');
 });
 
-// start server
-var server = app.listen(3000, function () {
-    console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
 });
